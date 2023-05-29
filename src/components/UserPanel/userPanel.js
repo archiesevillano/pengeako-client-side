93e4195781584d8c142ad9bfb6952a317d662620
jsarm052 <juliossrmnt@gmail.com>
2023-05-29 14:05:25 +0800

    update user

--- src/components/UserPanel/userPanel.js
+++ src/components/UserPanel/userPanel.js
@@ -52,7 +52,7 @@ const UserPanel = () => {
         <UserBtn iconName={"notifications"} count={797} />
       </div>
       <div className="right">
-        <a href="#" onClick={toggleFloatingNav}>
+        <a  onClick={toggleFloatingNav}>
           <img
             src="https://firebasestorage.googleapis.com/v0/b/pengeako-862f8.appspot.com/o/Images%2Fuser_placeholder.png?alt=media&token=830ea0d8-bbf9-4a68-b552-a6f6e313e6b1"
             alt="User"
